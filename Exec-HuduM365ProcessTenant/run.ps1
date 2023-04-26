140d50f39e4306351050f0325ad4452375ba1249
CIIPres <70212206+CIIPres@users.noreply.github.com>
2023-04-26 12:39:18 -0400

    Troubleshooting
    Added more information in the error code.

--- Exec-HuduM365ProcessTenant/run.ps1
+++ Exec-HuduM365ProcessTenant/run.ps1
@@ -317,7 +317,8 @@ try {
         try {
             $GroupMembersReturn = New-GraphBulkRequest -Headers $AuthHeaders -Requests $GroupRequestArray -tenantid $TenantFilter
         } catch {
-            $CompanyResult.Errors.add("Company $TenantFilter : Unable to fetch Group Membership Details for group $GroupRequestArray : $_")
+            $GroupRequestArrayList = $GroupRequestArray | Format-List
+            $CompanyResult.Errors.add("Company $TenantFilter : Unable to fetch Group Membership Details for group $GroupRequestArrayList : $_")
             $GroupMembersReturn = $null
         }
         $Groups = foreach ($Result in $GroupMembersReturn) {
